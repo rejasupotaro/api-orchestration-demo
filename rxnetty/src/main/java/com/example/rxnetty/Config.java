7b1d5a406804a59d69687ec525a8f0cff0b6bf49
Kentaro Takiguchi <takiguchi0817@gmail.com>
2014-10-12 22:38:34 +0900

    Define constants

--- rxnetty/src/main/java/com/example/rxnetty/Config.java
+++ rxnetty/src/main/java/com/example/rxnetty/Config.java
@@ -6,12 +6,14 @@ import io.reactivex.netty.protocol.http.server.HttpServer;
 import io.reactivex.netty.protocol.http.server.RequestHandler;
 
 public final class Config {
+    public static final String PROXY_HOST = "http://localhost:3000";
+    private static final int PORT = 8080;
     private static final Application APPLICATION = new Application();
 
     public static void main(String... args) throws InterruptedException {
         APPLICATION.run();
 
-        HttpServer<ByteBuf, ByteBuf> server = RxNetty.createHttpServer(8080, (request, response) -> {
+        HttpServer<ByteBuf, ByteBuf> server = RxNetty.createHttpServer(PORT, (request, response) -> {
             RequestHandler<ByteBuf, ByteBuf> route = APPLICATION.match(request.getPath());
             return route.handle(request, response);
         });
--- rxnetty/src/main/java/com/example/rxnetty/HttpProxy.java
+++ rxnetty/src/main/java/com/example/rxnetty/HttpProxy.java
@@ -7,6 +7,6 @@ import rx.Observable;
 
 public class HttpProxy {
     public static Observable<HttpClientResponse<ByteBuf>> get(String path) {
-        return RxNetty.createHttpGet("http://localhost:3000" + path);
+        return RxNetty.createHttpGet(Config.PROXY_HOST + path);
     }
 }
