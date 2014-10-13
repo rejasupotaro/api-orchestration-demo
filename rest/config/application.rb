88526cec57257a72f54a53f1ff8fb51e5568f6fc
Kentaro Takiguchi <takiguchi0817@gmail.com>
2014-10-13 10:58:23 +0900

    Show routes on application initialized

--- rest/config/application.rb
+++ rest/config/application.rb
@@ -8,16 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Rest
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.after_initialize do
+      Rails.application.reload_routes!
+      all_routes = Rails.application.routes.routes
+      require 'action_dispatch/routing/inspector'
+      inspector = ActionDispatch::Routing::RoutesInspector.new(all_routes)
+      puts inspector.format(ActionDispatch::Routing::ConsoleFormatter.new, ENV['CONTROLLER'])
+    end
   end
 end
