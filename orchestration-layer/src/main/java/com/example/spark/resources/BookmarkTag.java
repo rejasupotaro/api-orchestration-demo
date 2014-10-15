9d33a2ace473756ea399a9882a45dd381c944993
Kentaro Takiguchi <takiguchi0817@gmail.com>
2014-10-15 20:14:20 +0900

    Delete spark + rxjava

--- orchestration-layer/src/main/java/com/example/spark/resources/BookmarkTag.java
+++
@@ -1,8 +0,0 @@
-package com.example.spark.resources;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class BookmarkTag extends Resource {
-    @JsonProperty("id") private String id;
-    @JsonProperty("title") private String title;
-}
